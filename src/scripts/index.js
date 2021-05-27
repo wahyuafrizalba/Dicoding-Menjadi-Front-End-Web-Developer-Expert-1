50a3fbc205da13ae752e6bd72be40d990fb92681
Wahyu Afrizal Bahrul Alam <wahyuafrizal19@gmail.com>
2021-05-27 17:11:16 +0700

    Kurang aksesibilitas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,15 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import restaurants from '../DATA.json';
+
+// const json = require('../DATA.json');
 
 console.log('Hello Coders! :)');
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navbar-menu');
+const backgroundNavbar = document.querySelector('.background-navbar');
 const navbar = document.querySelector('.navbar');
 const navLink = document.querySelectorAll('.navbar-link');
 
@@ -30,3 +34,56 @@ hamburger.addEventListener('keydown', (event) => {
 });
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+const onScroll = () => {
+  const scroll = document.documentElement.scrollTop;
+
+  if (scroll > 0) {
+    backgroundNavbar.classList.add('scrolled');
+  } else {
+    backgroundNavbar.classList.remove('scrolled');
+  }
+};
+
+window.addEventListener('scroll', onScroll);
+
+restaurants.restaurants.forEach((restoran) => {
+  const article = document.createElement('article');
+
+  const image = document.createElement('img');
+  image.src = restoran.pictureId;
+  image.alt = restoran.name;
+  article.appendChild(image);
+
+  const rating = document.createElement('p');
+
+  const star = document.createElement('span');
+  star.className = 'fa fa-star checked';
+
+  rating.appendChild(star);
+
+  rating.innerHTML += ` ${restoran.rating}`;
+
+  article.appendChild(rating);
+
+  const text = document.createElement('div');
+  text.className = 'text';
+
+  const nama = document.createElement('h2');
+  nama.innerHTML = restoran.name;
+  text.appendChild(nama);
+
+  const kota = document.createElement('p');
+  kota.innerHTML = restoran.city;
+  kota.className = 'kota';
+  text.appendChild(kota);
+
+  const deskripsi = document.createElement('p');
+  deskripsi.innerHTML = restoran.description;
+  deskripsi.className = 'deskripsi';
+  text.appendChild(deskripsi);
+
+  article.appendChild(text);
+
+  document.getElementById('restoran').appendChild(article);
+});
